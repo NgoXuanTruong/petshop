6fe0d6bda74cf3f818cb7ea62ed70b0e88589e6d
Xuan-Truong <truong.xuan99419@gmail.com>
2021-11-02 21:11:44 +0700

    Clone

---
+++ initialize.php
@@ -0,0 +1,9 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://localhost/pet_shop/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"mysql5033.site4now.net");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"a7bea1_xtruong");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"99419");
+if(!defined('DB_NAME')) define('DB_NAME',"db_a7bea1_xtruong");
